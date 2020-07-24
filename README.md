3eaa4cc10ad55ee610ce9377654f1c8c55616a16
Luuk van Oijen <lazyluuk.channel@gmail.com>
2020-07-25 00:28:04 +0200

    vga output, interrupts, serial stuff, wild
    i should commit more lol

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # rxos
- thanks phil opp
+
+## Known issues
+- [ ] Double exceptions need to be fixed, they do not properly trigger.
+
+## Building
+### Windows
+`cargo build`
+`cargo bootimage --target x86_64-rxos.json`
+
+## Running
+### Windows
+`qemu-system-x86_64 -drive format=raw,file=target/x86_64-rxos/debug/bootimage-kernel.bin`
--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,19 +1,63 @@
-// src/main.rs
-
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
-use core::panic::PanicInfo;
+//Test stuff
+#![feature(custom_test_frameworks)]
+#![test_runner(kernel::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+use core::panic::PanicInfo;
+use kernel::{
+    print,
+    println,
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Error handling
+///////////////////////////////////////////////////////////////////////////////////////////////////
 /// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    kernel::test_panic_handler(info)
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Main function
+///////////////////////////////////////////////////////////////////////////////////////////////////
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
     // named `_start` by default
+
+    println!("Hello world!");
+
+    kernel::init();
+
+    //BREAKPOINT
+    // x86_64::instructions::interrupts::int3();
+
+    //Double fault
+    // unsafe {
+    //     *(0xdeadbeef as *mut u64) = 42;
+    // };
+
+    //Stack overflow
+    // fn stack_overflow() {
+    //     stack_overflow();
+    // }
+    // stack_overflow();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
     loop {}
 }
